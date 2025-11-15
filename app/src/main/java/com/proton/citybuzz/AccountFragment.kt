54bb106bd350c5c53a6f8453213fd58c115474f1
markovka <mark.mozh@gmail.com>
2025-11-15 21:13:27 +0100

    add loading screen

--- app/src/main/java/com/proton/citybuzz/AccountFragment.kt
+++ app/src/main/java/com/proton/citybuzz/AccountFragment.kt
@@ -14,6 +14,7 @@ import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.proton.citybuzz.data.SessionManager
 import com.proton.citybuzz.data.model.User
 import com.proton.citybuzz.ui.viewmodel.SocialViewModel
 
@@ -145,6 +146,7 @@ class AccountFragment : Fragment(R.layout.account_page) {
 
     fun logOut(){
         CityBuzzApp.getInstance().socialViewModel.loggedInUser.value = null
+        SessionManager.getInstance(requireContext()).logoutUser()
 
         parentFragmentManager.beginTransaction()
             .remove(this)
